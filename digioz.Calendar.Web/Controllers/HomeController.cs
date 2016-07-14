bcee10da23935816d97aef55216a4f2159334da2
psoheil <psoheil@ITAWSPSOHEIL>
2016-07-14 08:54:58 -0500

    Add project files.

---
+++ digioz.Calendar.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace digioz.Calendar.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? year, int? month)
+        {
+            if (year != null && month != null)
+            {
+                ViewBag.Year = year;
+                ViewBag.Month = month;
+            }
+
+            return View();
+        }
+    }
+}
